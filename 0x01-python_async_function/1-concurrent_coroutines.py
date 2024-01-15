d650bd5dfc3e6f4aeb2ab8e3beaf4d768e32a519
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-15 12:08:59 +0200

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
+""" 1. Let's execute multiple coroutines at the same time with async """
+
+import asyncio
+import random
 
 wait_random = __import__("0-basic_async_syntax").wait_random
-""" 1. Let's execute multiple coroutines at the same time with async """ ""
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> list:
     """Asynchronous coroutine that takes in an integer argument"""
     delays = []
     for i in range(n):
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-    for i in range(0, len(delays)):
-        for j in range(i + 1, len(delays)):
-            if delays[i] > delays[j]:
-                temp = delays[i]
-                delays[i] = delays[j]
-                delays[j] = temp
-    return delays
+        delays.append(await wait_random(max_delay))
+    return sorted(delays)
