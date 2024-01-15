5c86738973e17c357706f22d9973a7058578fe55
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-15 14:39:53 +0200

    4-tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,11 +3,12 @@
 """ 4. Tasks """
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that takes in an integer argument"""
     delays = []
     tasks = []
