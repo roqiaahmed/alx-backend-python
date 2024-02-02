3ffb6cc7f4a6e780e3d2fc95f8224bb7f19facc6
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-02 14:19:44 +0200

    test_org

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,6 @@ Parameterize a unit test
 """
 import unittest
 from client import GithubOrgClient
-from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from unittest.mock import patch
 
@@ -17,15 +16,15 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand(
         [
-            ("http://google.com", {"payload": True}),
-            ("http://abc.com", {"payload": False}),
+            ("google"),
+            ("abc"),
         ]
     )
-    @patch("test_client.get_json")
-    def test_org(self, url, pay_load, mock_get):
+    @patch("client.get_json")
+    def test_org(self, url, mock_get):
         """
         test_org
         """
-        mock_get.return_value = pay_load
-        get_json(url)
+        res = GithubOrgClient(url)
+        res.org
         mock_get.assert_called_once()
