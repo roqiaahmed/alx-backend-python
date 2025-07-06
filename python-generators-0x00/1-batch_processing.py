65a3da2681cd74b1d1439e100ef37839b6ec61ec
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-07 02:07:18 +0300

    Create a generator to fetch and process data

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -8,7 +8,7 @@ connection = seed.connect_to_prodev()
 
 def stream_users_in_batches(batch_size):
     cur = connection.cursor()
-    cur.execute("SELECT * from user_data")
+    cur.execute("SELECT * FROM user_data")
 
     con = True
     while con:
@@ -25,3 +25,4 @@ def batch_processing(batch_size):
         for user in batch:
             if user[3] >= 25:
                 print(user)
+                return user
