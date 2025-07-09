6f54baa5a22ff4481b30946baa9ddeaefcf59539
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-09 13:28:39 +0300

    retry database queries

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,42 @@
+import time
+import sqlite3
+import functools
+
+connection_module = __import__("1-with_db_connection")
+with_db_connection = connection_module.with_db_connection
+
+
+#### paste your with_db_decorator here
+def retry_on_failure(retries=3, delay=2):
+    def retry_on_failure_real(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = None
+            last_exception = None
+            for i in range(retries):
+                try:
+                    result = func(*args, **kwargs)
+                except sqlite3.DatabaseError as e:
+                    time.sleep(delay)
+                    last_exception = e
+            if result is None:
+                raise last_exception
+            return result
+
+        return wrapper
+
+    return retry_on_failure_real
+
+
+@retry_on_failure(retries=3, delay=1)
+@with_db_connection
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+
+#### attempt to fetch users with automatic retry on failure
+
+users = fetch_users_with_retry()
+print(users)
