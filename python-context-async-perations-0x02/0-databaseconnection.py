79507eff687da2db8d72d7cba9aef44fc2d07831
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-09 16:15:25 +0300

    create a class based context manager

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+class DatabaseConnection:
+    def __enter__(self):
+        self.connection = sqlite3.connect("users.db")
+        return self.connection
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close()
+
+
+with DatabaseConnection() as DC:
+    cursor = DC.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    print(results)
