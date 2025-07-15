5bd60194ed65b9a093dd1af81d18ec61a0c2ede3
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-15 12:44:57 +0300

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ module test
 """
 
 import unittest
+
 from parameterized import parameterized
 from utils import access_nested_map
 
@@ -19,7 +20,7 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2),
-        ],
+        ]
     )
     def test_access_nested_map(self, nested_map, path, expected):
         """
@@ -27,4 +28,4 @@ class TestAccessNestedMap(unittest.TestCase):
         """
 
         res = access_nested_map(nested_map, path)
-        self.assert_equal(res, expected)
+        self.assertEqual(res, expected)
