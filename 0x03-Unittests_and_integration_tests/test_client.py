de4020338f6d9c3efabf33943435578ddfe37632
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-15 22:39:54 +0300

    ..

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -33,39 +33,39 @@ class TestGithubOrgClient(unittest.TestCase):
         res.org()
         mock_get.assert_called_once()
 
+    def test_public_repos_url(self):
+        """
+        test_public_repos_url
+        """
+        with patch.object(
+            GithubOrgClient,
+            "org",
+            new_callable=PropertyMock,
+            return_value={"repos_url": "testURL"},
+        ) as mock_get:
+            client_json = {"repos_url": "testURL"}
+            client_repos_url = GithubOrgClient(client_json.get("repos_url"))
+            res = client_repos_url._public_repos_url
+            self.assertEqual(res, mock_get.return_value.get("repos_url"))
+            mock_get.assert_called_once
 
-#     def test_public_repos_url(self):
-#         """
-#         test_public_repos_url
-#         """
-#         with patch.object(
-#             GithubOrgClient,
-#             "org",
-#             new_callable=PropertyMock,
-#             return_value={"repos_url": "testURL"},
-#         ) as mock_get:
-#             client_json = {"repos_url": "testURL"}
-#             client_repos_url = GithubOrgClient(client_json.get("repos_url"))
-#             res = client_repos_url._public_repos_url
-#             self.assertEqual(res, mock_get.return_value.get("repos_url"))
-#             mock_get.assert_called_once
+    @patch("client.get_json", return_value={"name": "testName"})
+    def test_public_repos(self, mock_get):
+        """
+        test_public_repos
+        """
+        with patch.object(
+            GithubOrgClient,
+            "_public_repos_url",
+            new_callable=PropertyMock,
+            return_value="testURL",
+        ) as mock_pub:
+            client_pub_repo = GithubOrgClient("testName")
+            res_pub_repo = client_pub_repo.public_repos()
+            self.assertEqual(res_pub_repo, ["testName"])
+            mock_pub.assert_called_once
+            mock_get.assert_called_once
 
-#     @patch("client.get_json", return_value=[{"name": "testName"}])
-#     def test_public_repos(self, mock_get):
-#         """
-#         test_public_repos
-#         """
-#         with patch.object(
-#             GithubOrgClient,
-#             "_public_repos_url",
-#             new_callable=PropertyMock,
-#             return_value="testURL",
-#         ) as mock_pub:
-#             client_pub_repo = GithubOrgClient("testName")
-#             res_pub_repo = client_pub_repo.public_repos()
-#             self.assertEqual(res_pub_repo, ["testName"])
-#             mock_pub.assert_called_once
-#             mock_get.assert_called_once
 
 #     @parameterized.expand(
 #         [
