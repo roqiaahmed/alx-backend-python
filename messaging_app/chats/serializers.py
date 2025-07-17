95b8a18e437deda79b43b68bea80c3dca1383e51
Roqia Ahmed <roqiaahmed447@gmail.com>
2025-07-17 12:13:49 +0300

    add serializers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import User, Message, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversation
+        fields = "__all__"
